5896f9ee86087a9d968f333993f00818951adf68
mix <admin@yami.im>
2020-07-15 09:04:59 +0000

    :art: 持久化pixiv一图缓存

--- plugins/pixiv/__init__.py
+++ plugins/pixiv/__init__.py
@@ -13,7 +13,7 @@ from utils.message import processSession
 
 from .config import Config
 from .parse import parseMultiImage, parseSingleImage
-from .tools import downloadMutliImage, pixiv
+from .tools import cache, downloadMutliImage, pixiv
 
 __plugin_name__ = "pixiv"
 
@@ -169,11 +169,12 @@ def _(session: CommandSession):
 def _(session: CommandSession):
     global _RANK_CACHE
     session.send("开始获取一图")
-    if localtime().tm_hour >= 12:
-        randomRank = random.choice(["day", "week", "month"])
-        apiGet = pixiv.getRank(randomRank)
-        _RANK_CACHE[randomRank] = parseMultiImage(apiGet)
-    apiParse = _RANK_CACHE[random.choice(list(_RANK_CACHE))]
+    randomRank = random.choice(["day", "week", "month"])
+    apiParse = parseMultiImage(pixiv.getRank(randomRank))
+    if apiParse:
+        cache.update(randomRank, apiParse)
+    else:
+        apiParse = cache.get(randomRank)
     choiceResult = random.choice(
         [data for data in apiParse["result"] if data["type"] == "illust"]
     )
