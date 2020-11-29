bf2ac777a5c115644293f0ec6c0064b29b277dde
mix <admin@yami.im>
2020-11-30 00:27:38 +0800

    :construction: :building_construction: 建立基础文件结构

---
+++ utils/log.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from loguru import Logger
+from nonebot.log import logger as logger_
+
+from .config import Config
+
+LOG_PATH = Path(".") / "log"
+LOG_PATH.mkdir(parents=True, exist_ok=True)
+
+logger: Logger = logger_.opt(colors=True)
+logger.add(str(LOG_PATH / "{time}.log"), encoding="utf-8", enqueue=True)
+logger.level(Config["general"]["log"]["level"].as_str().upper())  # type:ignore
